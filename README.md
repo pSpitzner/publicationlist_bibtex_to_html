f571e5428933f3c674aa7dbd75e946c351e3cd8d
pSpitzner <paul.spitzner@ds.mpg.de>
2023-01-03 17:28:58 +0100

    added shields.io badges and mimicking the style for the abstract toggle button

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Create a html list of your publications
 based on an existing bibtex library.
 
+See a demo on my [website](https://makeitso.one/publications).
+
 ```
 pip install pylatexenc, bibtexparser
 ```
-
 
 One thing I am trying to do is work with arxiv.org ids to fetch altmetrics and create a
 badge automatically. This is a bit of a thing and afaik there is no uniform _right way_ how to get it consistent across zotero, bibtex, biblatex and csl.
@@ -13,3 +14,9 @@ My recommendation, for now:
 * use the [BetterBibtex plugin for zotero](https://retorque.re/zotero-better-bibtex/)
 * make sure that in the extra field you have the arxiv id in the format `arXiv:2007.03367`
 * BBT will pick this up and populate the `eprint` and `eprinttype` fields in the exported `.bib`
+
+# Changelog
+- 23-01-03: added badges from [shields.io](https://shields.io/)
+
+# ToDo:
+- similar to arxiv, we could automate badges for github etc. into the extra field of zotero and filter the corresponding bibtex entry.
--- bibtex_to_html.py
+++ bibtex_to_html.py
@@ -2,8 +2,11 @@
 # @Author:        F. Paul Spitzner
 # @Email:         paul.spitzner@ds.mpg.de
 # @Created:       2021-02-08 14:06:35
-# @Last Modified: 2022-06-09 15:46:19
+# @Last Modified: 2023-01-03 17:27:58
 # ------------------------------------------------------------------------------ #
+#
+# Github: https://github.com/pSpitzner/publicationlist_bibtex_to_html
+#
 # pip install pylatexenc bibtexparser
 #
 # Simple script to generate a html list of publications from an existing
@@ -14,6 +17,15 @@
 #
 # clean latex parsing for abstracts is still not there 100%
 # ------------------------------------------------------------------------------ #
+
+import logging
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)-8s | %(name)-12s | %(message)s",
+    datefmt="%y-%m-%d %H:%M",
+    level=logging.INFO,
+)
+log = logging.getLogger("main script")
+log.setLevel(logging.INFO)
 
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
@@ -37,15 +49,22 @@ show_abstracts = True
 # see https://api.altmetric.com/embeds.html
 # to get arxiv ids automatically, see readme
 show_altmetric = True
-
 show_arxiv_badge = True
+
+# check the badges at https://shields.io
+use_shieldsio_for_badges = True
+
+# use a button for abstract or plain text? better customize for your needs, below.
+use_bootstrap_button_for_abstract = True
+
+
 
 # this gets placed before/after the main publist that is created in main()
 page_prefix = """
 <!doctype html>
 <html lang="en">
 <head>
-<title>My Publications</title>
+    <title>My Publications</title>
 </head>
 
 <body>
@@ -69,10 +88,11 @@ def main():
         # do not skip non-standard bibtex fields
         parser.ignore_nonstandard_types = False
         db = bibtexparser.load(bibtex_file, parser=parser)
-    print(f"Found {len(db.entries)} entries in '{bibtex_path}'")
+    log.info(f"Found {len(db.entries)} entries in '{bibtex_path}'")
 
     # put these into the order in which they should appear online. customize below
     cite_keys = [
+        "neto_sampling_2022",
         "yamamoto_modular_2022",
         "hagemann_intrinsic_2022",
         "contreras_low_2021",
@@ -80,13 +100,12 @@ def main():
         "contreras_challenges_2021",
         "spitzner_mr_2021",
         "dehning_inferring_2020",
-        "neto_unified_2020",
         "spitzner_droplet_2018",
         "zierenberg_percolation_2017",
         "fricke_scaling_2017",
     ]
 
-    print(f"Fetching selected {len(cite_keys)} entries")
+    log.info(f"Fetching selected {len(cite_keys)} entries")
     entries = get_entry_for_citekey(db, cite_keys)
 
     # ------------------------------------------------------------------------------ #
@@ -99,22 +118,27 @@ def main():
 
     entries["yamamoto_modular_2022"]["year"] = "submitted"
     entries["yamamoto_modular_2022"]["journal"] = ""
+    entries["yamamoto_modular_2022"]["badges"] = [
+        {"desc": "GitHub", "url": "https://github.com/pSpitzner/stimulating_modular_cultures"},
+    ]
 
-    # badges should be a list of dicts, where each dict has 'name' and 'url'
+    # badges should be a list of dicts, where each dict has at least 'desc' and 'url'
+    # if using shilds io we get some defaults for github and arxiv,
+    # but you can also pass 'left', 'right', 'color' and 'logo'.
     entries["spitzner_mr_2021"]["badges"] = [
-        {"name": "GitHub", "url": "https://github.com/Priesemann-Group/mrestimator"},
+        {"desc": "GitHub", "url": "https://github.com/Priesemann-Group/mrestimator"},
     ]
 
 
     entries["dehning_inferring_2020"]["badges"] = [
-        {"name": "GitHub", "url": "https://github.com/Priesemann-Group/covid19_inference_forecast"},
+        {"desc": "GitHub", "url": "https://github.com/Priesemann-Group/covid19_inference_forecast"},
     ]
 
-    entries["neto_unified_2020"]["badges"] = [
-        {"name": "GitHub", "url": "https://github.com/Priesemann-Group/criticalavalanches"},
+    entries["neto_sampling_2022"]["badges"] = [
+        {"desc": "GitHub", "url": "https://github.com/Priesemann-Group/criticalavalanches"},
     ]
-    entries["neto_unified_2020"]["year"] = "under review"
-    entries["neto_unified_2020"]["journal"] = ""
+    # entries["neto_unified_2020"]["year"] = "under review"
+    # entries["neto_unified_2020"]["journal"] = ""
 
     # ------------------------------------------------------------------------------ #
     # or built custom entries from scratch. nested dict.
@@ -123,18 +147,21 @@ def main():
 
     other = {
         "msc_spitzner": {
+            "ID": "msc_spitzner",
             "author": "Spitzner, Franz Paul",
             "title": "Two Perspectives on the Condensation-Evaporation Transition of the Lennard-Jones Gas in 2D",
             "journal": "Master Thesis, Leipzig University",
             "url": "https://www.physik.uni-leipzig.de/~spitzner/publications/2017-spitzner-two_perspectives_on_the_condensation-evaporation_transition_of_the_lennard-jones_gas_in_2d.pdf",
         },
         "hauptseminar": {
+            "ID": "hs_spitzner",
             "author": "Spitzner, Franz Paul",
             "title": "Der Münchhausen-Trick — Pulling oneself up by one’s bootstrap",
             "journal": "Hauptseminar, Leipzig University",
             "url": "https://www.physik.uni-leipzig.de/~spitzner/publications/Spitzner_bootstrap.pdf",
         },
         "bsc_spitzner": {
+            "ID": "bsc_spitzner",
             "author": "Spitzner, Franz Paul",
             "title": "Generating Long-range Power-law Correlated Disorder",
             "journal": "Bachelor Thesis, Leipzig University",
@@ -145,7 +172,7 @@ def main():
     # ------------------------------------------------------------------------------ #
     # build html source code
     # ------------------------------------------------------------------------------ #
-    print(f"Building html")
+    log.info(f"Building html")
 
     html = ""
     html += page_prefix
@@ -171,9 +198,9 @@ def main():
     if output_path is not None and len(output_path) > 0:
         with open(output_path, "w") as text_file:
             text_file.write(html)
-        print(f"Output written to {output_path}")
+        log.info(f"Output written to {output_path}")
     else:
-        print("No ouput_path set, not writing any output. Edit this script!")
+        log.info("No ouput_path set, not writing any output. Edit this script!")
 
 
 # fmt:off
@@ -182,13 +209,21 @@ def entry_to_html(entry):
         Converts the entry dict for every publication into formatted html.
         Customize as needed to change the style of every list item.
     """
+
+    log.debug(f"Formatting {entry}")
+
+    # we will print this as info, below.
+    debug_string = f"{entry['ID'] : >30} formatted with:  "
+
+    # here we collect the final html
     html = ""
 
-    # make it a list item
+    # make it a list item, since references are usually a list
     html += "<li>\n"
 
     # format authors
     if 'author' in entry and len(entry['author']) > 0:
+        debug_string += "authors "
         html += '<div class="pub_author">\n'
         html += format_authors(entry)
         # html += ':'
@@ -196,20 +231,25 @@ def entry_to_html(entry):
 
     # format titles as links that lead to the url
     if 'title' in entry and len(entry['title']) > 0:
-        html += '<a class="pub_title"\n'
-        if 'url' in entry and len(entry['url']) > 0:
+        debug_string += "title "
+        url_is_okay = ('url' in entry and len(entry['url']) > 0)
+        html += f'<{"a" if url_is_okay else "div"} class="pub_title"\n'
+        if url_is_okay:
+            debug_string += "url "
             html += 'href="' + entry['url'] + '"\n'
         html += '>\n'
         # title
         html += cleanup(entry['title'])
         # html += ','
-        html += '\n</a>\n'
+        html += f'\n</{"a" if url_is_okay else "div"}>\n'
+
 
     # I wanted journal, year and badges in one line -> spans enclosed in one div
     html += '<div class="pub_journal_group">\n'
 
     # journal, volume, pages
     if 'journal' in entry and len(entry['journal']) > 0:
+        debug_string += "journal "
         html += '<span class="pub_journal">\n'
         html += entry['journal']
         if 'volume' in entry and len(entry['volume']) > 0:
@@ -220,6 +260,7 @@ def entry_to_html(entry):
 
     # year, only show this if we have a journal and not a preprint
     if 'year' in entry and len(entry['year']) > 0:
+        debug_string += "year "
         html += '<span class="pub_year">\n'
         html += '(' + entry['year'] + ')'
         html += '\n</span>\n'
@@ -230,37 +271,43 @@ def entry_to_html(entry):
 
     # abstract badge first (toggles whether the abstract is displayed or not)
     if show_abstracts and 'abstract' in entry and len(entry['abstract']) > 0:
-        html += '<span class="pub_badge">\n'
-        html += '['
-        html += '<span class="fake_a pub_badge_link" '
-        html += 'data-toggle="collapse" aria-expanded="false" '
-        html += 'data-target="#abstract_' + entry['ID'] + '">'
-        html += "Abstract"
-        html += '</span>'
-        html += ']\n'
-        html += '</span>\n'
+        debug_string += "abstract "
 
+        if use_bootstrap_button_for_abstract:
+            html += '<span class="pub_badge">\n'
+            html += '<span type="button" class="btn" '
+            html += 'data-toggle="collapse" aria-expanded="false" '
+            html += 'data-target="#abstract_' + entry['ID'] + '">'
+            html += "Toggle abstract"
+            html += '</span>'
+            html += '</span>\n'
 
-    # badges for arxiv and the likes
-    def format_badge(name, url):
-        badge = '<span class="pub_badge">\n'
-        badge += '['
-        badge += '<a class="pub_badge_link"'
-        badge += 'href="' + url + '">'
-        badge += name
-        badge += '</a>'
-        badge += ']'
-        badge += '\n</span>\n'
-        return badge
+        else:
+            html += '<span class="pub_badge">\n'
+            html += '['
+            html += '<span class="fake_a pub_badge_link" '
+            html += 'data-toggle="collapse" aria-expanded="false" '
+            html += 'data-target="#abstract_' + entry['ID'] + '">'
+            html += "Abstract"
+            html += '</span>'
+            html += ']\n'
+            html += '</span>\n'
 
-    if 'badges' in entry and len(entry['badges']) > 0:
-        for b in entry['badges']:
-            assert 'url' in b and 'name' in b
-            html += format_badge(b['name'], b['url'])
 
     # arxiv badge
     if show_arxiv_badge and "arxiv_org_id" in entry:
-        html += format_badge("arXiv", "https://arxiv.org/abs/" + entry["arxiv_org_id"])
+        html += format_badge(
+            desc = "arXiv",
+            url = "https://arxiv.org/abs/" + entry["arxiv_org_id"],
+        )
+        debug_string += f"badge(arXiv) "
+
+    # otther badges
+    if 'badges' in entry and len(entry['badges']) > 0:
+        for b in entry['badges']:
+            assert 'url' in b and 'desc' in b
+            html += format_badge(**b)
+            debug_string += f"badge({list(b.values())[0]}) "
 
     # altmetric badge
     if show_altmetric and ("arxiv_org_id" in entry or "doi" in entry):
@@ -273,6 +320,7 @@ def entry_to_html(entry):
         else:
             html += f'data-doi="{entry["doi"]}"'
         html += ' ></span>\n'
+        debug_string += f"badge(altmetric) "
 
     html += '</div>\n' # journal group
 
@@ -285,6 +333,8 @@ def entry_to_html(entry):
         html += '</div>'
 
     html += '</li>\n\n' # list item
+
+    log.info(debug_string)
 
     return html
 # fmt:on
@@ -399,6 +449,53 @@ def get_entries_for_author(db, author_strings):
     return res
 
 
+def format_badge(**kwargs):
+# badges for arxiv and the likes, pass at least 'desc' and 'url'
+    if use_shieldsio_for_badges:
+        badge = '<span class="pub_badge">\n'
+        badge += '<a class="pub_badge_link"'
+        badge += 'href="' + kwargs['url'] + '">'
+        badge += '<img src="'
+
+        # prepate shildio badges
+        if len(kwargs) == 2 and 'desc' in kwargs and 'url' in kwargs:
+            # default case, use our own defaults
+            if 'arxiv' == kwargs['desc'].lower():
+                # this is a bit back and forth, but keeps it in one place
+                arxiv_id = kwargs['url'].replace("https://arxiv.org/abs/", "")
+                badge += shieldio(left='arXiv', right=arxiv_id, color='b31b1b')
+            elif 'github' == kwargs['desc'].lower():
+                badge += shieldio(left='', right='GitHub', color='066da5', logo='github')
+        else:
+            # pass what we got
+            badge += shieldio(**kwargs)
+
+        badge += '"></img>'
+        badge += '</a>'
+        badge += '\n</span>\n'
+    else:
+        badge = '<span class="pub_badge">\n'
+        badge += '['
+        badge += '<a class="pub_badge_link"'
+        badge += 'href="' + kwargs['url'] + '">'
+        badge += kwargs['desc']
+        badge += '</a>'
+        badge += ']'
+        badge += '\n</span>\n'
+    return badge
+
+def shieldio(left, right, color=None, logo=None, logoColor=None):
+    # format url to get the shield.io image
+    url = "https://img.shields.io/badge/"
+    url += left + "-" + right
+    if color is not None:
+        url += "-" + color
+    if logo is not None:
+        url += "?logo=" + logo
+        if logoColor is not None:
+            url += "&logoColor=" + logoColor
+    return url
+
 def format_authors(entry, abbreviate_first=True, et_al_at=1000):
     """
         this is the way i like it, tweak as needed.
@@ -414,7 +511,7 @@ def format_authors(entry, abbreviate_first=True, et_al_at=1000):
     for name in names:
         # {'first': ['F.', 'Paul'], 'last': ['Spitzner'], 'von': [], 'jr': []}
         split = btxc.splitname(name)
-        # print(split)
+        # log.info(split)
         if not abbreviate_first:
             first = " ".join(split["first"])
         else:
@@ -426,7 +523,7 @@ def format_authors(entry, abbreviate_first=True, et_al_at=1000):
                 elif f[1] in ".:;":
                     first += f[0] + "."
                 else:
-                    print(
+                    log.info(
                         f"Adapt the `format_authors` script to your needs for entry {r['ID']}"
                     )
 
